edbbcf7e9457d753d1dd8f4d4870d8246fa3c68b
kclowes <kclowes@users.noreply.github.com>
2024-08-05 11:34:05 -0600

    Test wrong code behaves as expected

--- tests/test_faucet.py
+++ tests/test_faucet.py
@@ -1,5 +1,6 @@
 import pytest
 from ape import project, convert
+from ape.exceptions import ContractLogicError
 from web3 import Web3
 
 drip_amount = convert("0.0001 ETH", int)
@@ -19,8 +20,13 @@ def test_faucet_drip(acct1, acct2, contract_faucet):
     assert acct2.balance == original_balance + drip_amount
 
 
-def test_no_drip_if_no_code():
-    pass
+def test_no_drip_if_no_code(acct1, acct2, contract_faucet):
+    original_balance = acct2.balance
+    contract_faucet.seedFunds("devcon", sender=acct1, value=drip_amount * 10)
+    with pytest.raises(ContractLogicError):
+        contract_faucet.drip(acct2, "wrong code", sender=acct1)
+    assert contract_faucet.eventFunds(hashed_code) == drip_amount * 10
+    assert acct2.balance == original_balance
 
 
 def test_owner_can_withdraw():
