2eaf5ee4eed2195bd327e7ed1e2ba8c3cfd5ef68
kclowes <kclowes@users.noreply.github.com>
2024-08-05 11:44:29 -0600

    A few more tests

--- tests/test_faucet.py
+++ tests/test_faucet.py
@@ -1,6 +1,7 @@
 import pytest
 from ape import project, convert
 from ape.exceptions import ContractLogicError
+
 from web3 import Web3
 
 drip_amount = convert("0.0001 ETH", int)
@@ -29,9 +30,18 @@ def test_no_drip_if_no_code(acct1, acct2, contract_faucet):
     assert acct2.balance == original_balance
 
 
-def test_owner_can_withdraw():
-    pass
+def test_owner_can_withdraw(acct1, contract_faucet):
+    original_contract_balance = contract_faucet.balance
+    original_owner_balance = acct1.balance
+    contract_faucet.seedFunds("withdrawal", sender=acct1, value=drip_amount * 5)
+    contract_faucet.withdraw(sender=acct1)
+    assert contract_faucet.balance == 0
+    # assert acct1.balance == original_owner_balance + (drip_amount * 5)
 
 
-def test_not_owner_cannot_withdraw():
-    pass
+def test_not_owner_cannot_withdraw(acct1, acct2, contract_faucet):
+    original_contract_balance = contract_faucet.balance
+    contract_faucet.seedFunds("withdrawal", sender=acct1, value=drip_amount * 5)
+    with pytest.raises(ContractLogicError):
+        contract_faucet.withdraw(sender=acct2)
+    assert contract_faucet.balance == drip_amount * 5
