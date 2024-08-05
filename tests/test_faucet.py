6c3cedf786d9c957fc18945dec1dc2b6e5529bae
kclowes <kclowes@users.noreply.github.com>
2024-08-05 11:28:57 -0600

    Hash event string

--- tests/test_faucet.py
+++ tests/test_faucet.py
@@ -1,17 +1,31 @@
 import pytest
 from ape import project, convert
+from web3 import Web3
 
 drip_amount = convert("0.0001 ETH", int)
+hashed_code = Web3.keccak(text="devcon")
 
 
 def test_faucet_creation(acct1, contract_faucet):
     contract_faucet.seedFunds("devcon", sender=acct1, value=drip_amount * 10)
-    assert contract_faucet.eventPayments("devcon") == drip_amount * 10
+    assert contract_faucet.eventFunds(hashed_code) == drip_amount * 10
 
 
 def test_faucet_drip(acct1, acct2, contract_faucet):
     original_balance = acct2.balance
     contract_faucet.seedFunds("devcon", sender=acct1, value=drip_amount * 10)
     contract_faucet.drip(acct2, "devcon", sender=acct1)
-    assert contract_faucet.eventPayments("devcon") == drip_amount * 9
+    assert contract_faucet.eventFunds(hashed_code) == drip_amount * 9
     assert acct2.balance == original_balance + drip_amount
+
+
+def test_no_drip_if_no_code():
+    pass
+
+
+def test_owner_can_withdraw():
+    pass
+
+
+def test_not_owner_cannot_withdraw():
+    pass
