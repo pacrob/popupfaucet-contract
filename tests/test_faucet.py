7ab89ddf50985f0560dea27ac63a66cce76a8b2e
wolovim <3621728+wolovim@users.noreply.github.com>
2024-08-05 12:00:18 -0400

    basic drip test and contract path fix

--- tests/test_faucet.py
+++ tests/test_faucet.py
@@ -1,9 +1,17 @@
 import pytest
-from ape import project
+from ape import project, convert
+
+drip_amount = convert("0.0001 ETH", int)
 
 
-def test_faucet_creation(acct1, acct2, contract_faucet):
-    contract_faucet.seedFunds("devcon", sender=acct1, value=10**18)
-    assert contract_faucet.eventPayments("devcon") == 10**18
+def test_faucet_creation(acct1, contract_faucet):
+    contract_faucet.seedFunds("devcon", sender=acct1, value=drip_amount * 10)
+    assert contract_faucet.eventPayments("devcon") == drip_amount * 10
+
+
+def test_faucet_drip(acct1, acct2, contract_faucet):
+    original_balance = acct2.balance
+    contract_faucet.seedFunds("devcon", sender=acct1, value=drip_amount * 10)
     contract_faucet.drip(acct2, "devcon", sender=acct1)
-    assert contract_faucet.eventPayments("devcon") == 999900000000000000
+    assert contract_faucet.eventPayments("devcon") == drip_amount * 9
+    assert acct2.balance == original_balance + drip_amount
