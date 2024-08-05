f8738b38c407d840305a3df7bbd630c60c1f21cf
kclowes <kclowes@users.noreply.github.com>
2024-08-05 08:58:17 -0600

    Drip tested

--- tests/test_faucet.py
+++ tests/test_faucet.py
@@ -3,7 +3,7 @@ from ape import project
 
 
 def test_faucet_creation(acct1, acct2, contract_faucet):
-    contract_faucet.seedFunds("devcon", sender=acct1, value=100)
-    assert contract_faucet.eventPayments("devcon") == 100
-    # contract_faucet.drip(acct2, "devcon", sender=acct1)
-    # assert contract_faucet.eventPayments("devcon") == (100 - 0.0001)
+    contract_faucet.seedFunds("devcon", sender=acct1, value=10**18)
+    assert contract_faucet.eventPayments("devcon") == 10**18
+    contract_faucet.drip(acct2, "devcon", sender=acct1)
+    assert contract_faucet.eventPayments("devcon") == 999900000000000000
